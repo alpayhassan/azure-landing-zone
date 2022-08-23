f17dd203a6fd12d03c1c398a150f6c2ce871c2dd
alpayhassan1 <alpayhassan@hotmail.com>
2022-08-23 19:06:40 +0100

    route table adjustment

--- route-tables.tf
+++ route-tables.tf
@@ -1,6 +1,6 @@
 # Routing from Corp Landing Zone to the office network (on-premise)
 resource "azurerm_route_table" "route-table-corp-to-office" {
-  provider = azurerm.corp-sub
+  provider                      = azurerm.corp-sub
   name                          = "corp2office"
   location                      = azurerm_resource_group.corp-rg.location
   resource_group_name           = azurerm_resource_group.corp-rg.name
