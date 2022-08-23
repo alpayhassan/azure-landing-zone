3e368dd3cd57d43ff6e2c9abc78cf1e7cf7a3de0
alpayhassan1 <alpayhassan@hotmail.com>
2022-08-23 19:05:49 +0100

    changed sku to vnet from hub

--- firewall.tf
+++ firewall.tf
@@ -22,7 +22,7 @@ resource "azurerm_firewall" "firewall" {
   name                = "firewall"
   location            = local.fw-location
   resource_group_name = azurerm_resource_group.connectivity-rg.name
-  sku_name            = "AZFW_Hub"
+  sku_name            = "AZFW_VNet"
   sku_tier            = "Premium"
   firewall_policy_id  = azurerm_firewall_policy.firewall-policy.id
 
